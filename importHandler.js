ecda922f8e72f78d64e65813f89aad7c61dd3c65
pedrobelmino <pedrobelmino@gmail.com>
2021-04-21 20:58:40 -0300

    Update importHandler.js

--- importHandler.js
+++ importHandler.js
@@ -1,4 +1,3 @@
-console.log('Loading function');
 'use strict';
 
 const aws = require('aws-sdk');
@@ -6,9 +5,23 @@ const aws = require('aws-sdk');
 const s3 = new aws.S3({ apiVersion: '2006-03-01' });
 
 module.exports.importDBZJsonToDynamodDB = async (event, context, callback) => {
-  const records = event.Records;
-
   console.log(event);
-
-  
+  const bucket = event.Records[0].s3.bucket.name;
+  const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+  const params = {
+      Bucket: bucket,
+      Key: key,
+  };
+  console.log()
+  s3.getObject(params, (err, data) => {
+      if (err) {
+          console.log(err);
+          const message = `Error getting object ${key} from bucket ${bucket}. Make sure they exist and your bucket is in the same region as this function.`;
+          console.log(message);
+          callback(message);
+      } else {
+          console.log('CONTENT TYPE:', data.ContentType);
+          callback(null, data.ContentType);
+      }
+  });
 };
